09fbf5a2548fb7bd3d61ff45202a409c5d7d9f2d
jamik-dev <jamik.oik.group@gmail.com>
2022-06-13 18:26:39 +0500

    changes

--- data/config.py
+++ data/config.py
@@ -8,7 +8,7 @@
 # BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 # ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 # IP = env.str("ip")  # Xosting ip manzili
-# CHANNELS = ["@IT_memes_eng"]    
+CHANNELS = ["@IT_memes_eng"]    
 
 import os
 BOT_TOKEN=str(os.environ.get("BOT_TOKEN"))
